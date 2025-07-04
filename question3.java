0023ef513eb9e83ad6b65670a4774ce1c1e39131
mokshitha2306 <mokshithareddy236@gmail.com>
2025-07-04 16:56:06 +0530

    javafundamentalsq3

---
+++ question3.java
@@ -0,0 +1,18 @@
+package javafundamentals;
+
+public class question3 {
+	public static void main(String[] args) {
+		if (args.length != 2) {
+            System.out.println("Please provide exactly two integers");
+            return;
+        }
+        try {
+            int num1 = Integer.parseInt(args[0]);
+            int num2 = Integer.parseInt(args[1]);
+            int sum = num1 + num2;
+            System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid input. Please enter two valid integers.");
+        }
+    }
+}
